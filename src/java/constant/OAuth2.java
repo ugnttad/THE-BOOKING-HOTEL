99fc24ad06b63d41f7d76d1a0c7cabbca823e937
ungttad <dat200120040@gmail.com>
2025-04-04 00:10:22 +0700

    all of the code fix

--- src/java/constant/OAuth2.java
+++ src/java/constant/OAuth2.java
@@ -2,8 +2,6 @@ package constant;
 
 public class OAuth2 {
 
-//    public static String GOOGLE_CLIENT_ID = "151505397859-kolkm3d4fmvi21gkvjfdkmj3jufcpht5.apps.googleusercontent.com";
-//    public static String GOOGLE_CLIENT_SECRET = "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";
     public static String GOOGLE_REDIRECT_URI = "http://localhost:8080/HotelBooking/google-login";
     public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
     public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
